4fef0e35ae467f27c2b8db12b6705eefad5b8dd5
Coleton Gorecke <goreckec@gmail.com>
2021-07-17 22:17:14 -0500

    refactored card widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const primaryColor = Color(0xFF1D1E33);
@@ -21,16 +22,39 @@ class _InputPageState extends State<InputPage> {
         body: Column(
           children: [
             Expanded(
-                child: Row(
-              children: [
-                Expanded(
-                  child: ReusableCard(color: primaryColor),
-                ),
-                Expanded(
-                  child: ReusableCard(color: primaryColor),
-                ),
-              ],
-            )),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(
+                      color: primaryColor,
+                      cardChild: Container(
+                        child: Text('Test Text'),
+                      ),
+                      // cardChild: Column(
+                      //   mainAxisAlignment: MainAxisAlignment.center,
+                      //   children: [
+                      //     Icon(FontAwesomeIcons.mars, size: 80.0),
+                      //     SizedBox(
+                      //       height: 15.0,
+                      //     ),
+                      //     Text(
+                      //       'MALE',
+                      //       style: TextStyle(
+                      //         fontSize: 18.0,
+                      //         color: Color(0xFF8D8E98),
+                      //       ),
+                      //     ),
+                      // ],
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      color: primaryColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
             Expanded(
               child: ReusableCard(color: primaryColor),
             ),
@@ -60,9 +84,10 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.color});
+  ReusableCard({@required this.color, this.cardChild});
 
   final Color color;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
